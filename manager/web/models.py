fa0689cbe017324faceed78bba37024c0dbebad1
yorling <shallowcloud@yeah.net>
2018-05-19 10:45:15 +0800

    add data models for web manager and update README.

--- manager/web/models.py
+++ manager/web/models.py
@@ -1,9 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    id = models.AutoField("ID", primary_key=True)
 
 
 class ConfigFile(models.Model):
 
-    pass
+    STATUS_CHOICES = (
+        ("正常", "该配置文件正常"),
+        ("未完成", "该配置文件未配置文成"),
+        ("停用", "该配置文件已被停用"),
+    )
+
+    id = models.AutoField("ID", primary_key=True)
+    name = models.CharField("name", max_length=30, default="Unnamed")
+    status = models.CharField("status", max_length=20, choices=STATUS_CHOICES, default="正常")
+    contents = models.TextField("contents", default="")
+    create_time = models.DateTimeField("create time", default=timezone.now)
+
+
+class Server(models.Model):
+
+    STATUS_CHOICES = (
+        ("正常", "该节点与服务器连接正常"),
+        ("连接断开", "该节点与服务器连接已断开"),
+        ("未连接", "该节点未与服务器建立连接"),
+    )
+
+    id = models.AutoField("ID", primary_key=True)
+    ip_address = models.GenericIPAddressField("ip address", protocol="IPv4", default="127.0.0.1")
+    status = models.CharField("status", max_length=20, choices=STATUS_CHOICES, default="未连接")
+
+
+    
 
--- manager/web/views.py
+++ manager/web/views.py
@@ -13,8 +13,9 @@ from manager import const
 
 class WebListView(View):
 
+    @method_decorator(login_required(login_url="WebLogin"))
     def get(self, request):
-        return render(request, "func/list.html", {"sources":{"title":"服务器列表"}})
+        return render(request, "func/list.html", {"sources": {"title": "服务器列表"}})
 
 
 class WebLoginView(View):
@@ -32,15 +33,19 @@ class WebLoginView(View):
             auth_res  = auth.authenticate(request, username=request.POST["username"], password=request.POST["password"])
             if auth_res and auth_res.is_active:
                 auth.login(request, auth_res)
-                return HttpResponseRedirect(reverse("WebList"))
+                if "next" in request.GET:
+                    return HttpResponseRedirect(request.GET["next"])
+                else:
+                    return HttpResponseRedirect(reverse("WebList"))
             else:
                 return render(request, "user/login.html", {"error":[const.LOGIN_ERROR_TIP]})
         else:
-            return render(request, "user/login.html", {"error":[const.LOGIN_ERROR_EMPTY_TIP]})
+            return render(request, "user/login.html", {"error": [const.LOGIN_ERROR_EMPTY_TIP]})
 
 
 class WebLogoutView(View):
 
+    @method_decorator(login_required(login_url="WebLogin"))
     def get(self, request):
         auth.logout(request)
         return HttpResponseRedirect(reverse("WebLogin"))
