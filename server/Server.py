cdb67102acf5db8690ef03003687a7ea2860b564
Riley Wen <wrllrwwrllrw@gmail.com>
2018-10-18 17:20:53 +0800

    Add final main loop.

--- server/Server.py
+++ server/Server.py
@@ -15,6 +15,7 @@ def timethis(func):
 
 
 def main(general_settings: dict):
+    """
     attr_get = {
         "type": "GET",
         "uuid": "1be317fc-c871-11e8-89be-3cf862da4116",
@@ -49,11 +50,11 @@ def main(general_settings: dict):
         "file_list": [
             {
                 "remote_path": "/home/rileywen/Documents/Test/get2.txt",
-                "file-content-b64": "<base64 encode>"
+                "file_content_b64": "MTExMTExCg=="
             },
             {
                 "remote_path": "/home/rileywen/Documents/Test/get1.txt",
-                "file-content-b64": "<base64 encode>"
+                "file_content_b64": "VGhpcyBpcyBmaWxlIDIhISEhIQ=="
             },
         ],
     }
@@ -61,27 +62,26 @@ def main(general_settings: dict):
 
     Logger.info(attr_get)
     Logger.info(client_list)
+    """
+    queue = redis.Redis()
+    try:
+        while True:
+            task = json.loads(queue.blpop('task'))
+            client_list = task.pop("client_list")
+            result = WebHandlers.pass_attrs_to_clients(task, client_list, general_settings['key'],
+                                                       general_settings['timeout'])
+            queue.lpush('result', json.dumps(result))
+    except Exception as e:
+        Logger.info('Error occurred within Main Loop:\n', str(e))
 
     """
-    attrs = {
-        'method': 'check_conn',
-        'bool': True,
-        'local-path': 'C:/Users/76033/Desktop/Test/Server/FireShot Capture 4.png',
-        'remote-path': 'C:/Users/76033/Desktop/Test/Client/FireShot Capture 4.png',
-        'timeout': 4
-    }
-    client_list = [
-        'google.com',
-        '127.0.0.1'
-    ]
-    """
-
     key = DataPacking.get_key32()
     key_ = b'\x0c@\xf0\x0f +\xd1g\x84\xf1#Z\xc3\xe4\xabX|\xe7\xa4\x00\x94\xc5{\x0eS\x8e\x1f\x1e\x07\xd0eh'
-    ret_val = WebHandlers.pass_attrs_to_clients(attr_get, client_list, general_settings['key'],
+    ret_val = WebHandlers.pass_attrs_to_clients(attr_post, client_list, general_settings['key'],
                                                 general_settings['timeout'])
     Logger.info("Concatenated result of all subprocess: ", ret_val, level=Logger.DEBUG)
     pprint(ret_val)
+    """
 
 
 if __name__ == '__main__':
@@ -96,6 +96,7 @@ if __name__ == '__main__':
     import time
     import json
     import base64
+    import redis
     from pprint import pprint
     from server import WebHandlers
     from server.utils import DataPacking
